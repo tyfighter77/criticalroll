ab81e997a87b21ea9d38dc9a06607263efb7c074
Ty Hill <crazysnoheadman@yahoo.com>
2016-04-14 14:41:16 -0600

    started backend

---
+++ server/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(__direname + './../public'));
+
+var port = 3000;
+var mongoURI = 'mongodb://localhost:27017/criticalroll';
+
+mongoose.set('debug', true);
+mongoose.connect(mongoURI);
+mongoose.connection.once('open', function() {
+  console.log('Connected to Mongo at ', mongoURI);
+});
+
+app.listen(port, function() {
+  console.log('Listening on port ', port);
+});
